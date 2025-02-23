ab3699347e920c008ca467e560d53658862be6a2
Calvin Li <cjli4@uci.edu>
2025-02-22 19:16:28 -0800

    Scraping text from files
    Taking the HTML content from the .json files and retrieving the text from them, making a differentiation between bold/header/title tags and regular text.

--- processor.py
+++ processor.py
@@ -1,5 +1,6 @@
 import re
 import nltk
+from lxml import etree
 
 def tokenize(text):
     # Match alphanumeric sequences as tokens
@@ -9,3 +10,16 @@ def porter_stem(tokens):
     # Initialize porter stemmer
     stemmer = nltk.PorterStemmer()
     return [stemmer.stem(token) for token in tokens]
+
+def retrieve_important(tree: etree.ElementTree):
+    # Retrieve the content from the bold, header, and title tags
+    return ''.join(tree.xpath("//b|//title|//h"))
+
+def retrieve_content(tree: etree.ElementTree):
+    # Remove the bold, header, and title tags
+    to_drop = tree.xpath('//b|//title|//h')
+    for tag in to_drop:
+        tag.getparent().remove(tag)
+
+    # Get all text content
+    return ''.join(tree.xpath("//text()"))
