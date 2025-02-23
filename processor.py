26371c2dd0d2df42968047c55ff95159c0a40fec
Calvin Li <cjli4@uci.edu>
2025-02-22 19:30:09 -0800

    Adjusted return values of content retrieval
    Retrieving the content now returns tokens, and the retrieving of special/important tokens just returns the set of unique tokens that are important, so that we can mark that as a flag later in the index structure

--- processor.py
+++ processor.py
@@ -12,14 +12,9 @@ def porter_stem(tokens):
     return [stemmer.stem(token) for token in tokens]
 
 def retrieve_important(tree: etree.ElementTree):
-    # Retrieve the content from the bold, header, and title tags
-    return ''.join(tree.xpath("//b|//title|//h"))
+    # Retrieve the unique tokens from the bold, header, and title tags
+    return set(tokenize(''.join(tree.xpath("//b|//title|//h"))))
 
 def retrieve_content(tree: etree.ElementTree):
-    # Remove the bold, header, and title tags
-    to_drop = tree.xpath('//b|//title|//h')
-    for tag in to_drop:
-        tag.getparent().remove(tag)
-
     # Get all text content
-    return ''.join(tree.xpath("//text()"))
+    return tokenize(''.join(tree.xpath("//text()")))
