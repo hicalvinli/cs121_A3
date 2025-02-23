2d9e732817b0ed76938fa5fdd9c064cc6ae291aa
Maggie Zhang <maggiegz@uci.edu>
2025-02-22 22:13:52 -0800

    writing index buckets to corresponding files

--- processor.py
+++ processor.py
@@ -4,6 +4,8 @@ import json
 import ijson
 from lxml import etree
 
+FINAL = dict()
+
 def _tokenize(text: str) -> list[str]:
     # Match alphanumeric sequences as tokens
     return re.findall(r"[a-zA-Z\d]+", text)
@@ -33,21 +35,39 @@ def partial_indexer(indexer: dict, file_count: int) -> None:
     with open(f"indexed_{file_count}", "w") as f:
         json.dump(indexer, f)
 
+def write_sub_final(bucket: list):
+    global FINAL
+    with open(f"index_{str(bucket[0])}-{str(bucket[-1])}", "w") as f:
+        json.dump(FINAL, f)
+    FINAL = dict()
+
+def _iter_tokens(chr: str, tokens):
+    global FINAL
+    for token, sub_info in tokens:
+        if token.startswith(chr):
+            total_info = FINAL.get(token, {})
+            FINAL[token] = total_info.add(sub_info)
+
 
 def _split_indexes(pindex: int, bucket: tuple) -> None:
     # Splits indexes based on range.
-    final_ind = dict()
+    global FINAL
     with open(f"indexed_{pindex}", 'r') as f:
         for chr in bucket:
-            token = ijson.kvitems(f, '')
-        # ijson turns partial index into an iterator, avoiding reading
-        # it into main memory all at once.
-
+            # ijson turns partial index into an iterator, avoiding reading
+            # it into main memory all at once.
+            tokens = ijson.kvitems(f, '')
+            _iter_tokens(chr, tokens)
 
 
 def merge_indexes(file_num: int) -> None:
     # Merges all partial indexes alphabetically, writing each partial into a split range index
-    index_buckets = [(0, 9), ('a', 'g'), ('h', 'o'), ('p', 'z')]
+    global FINAL
+    index_buckets = [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+                     ('a', 'b', 'c', 'd', 'e', 'f', 'g'),
+                     ('h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'),
+                     ('p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')]
     for bucket in index_buckets:
         for pindex in range(1, file_num + 1):
             _split_indexes(file_num, bucket)
+            FINAL = _alpha_sort(FINAL)
