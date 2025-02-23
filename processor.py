dfa29386d2ad9f5c4cca18f782ae16ecbacfe851
Calvin Li <cjli4@uci.edu>
2025-02-22 19:45:09 -0800

    Stem the important tokens before putting them in the set

--- processor.py
+++ processor.py
@@ -2,19 +2,19 @@ import re
 import nltk
 from lxml import etree
 
-def tokenize(text):
+def tokenize(text: str) -> list[str]:
     # Match alphanumeric sequences as tokens
     return re.findall(r"[a-zA-Z\d]+", text)
 
-def porter_stem(tokens):
+def porter_stem(tokens: list[str]) -> list[str]:
     # Initialize porter stemmer
     stemmer = nltk.PorterStemmer()
     return [stemmer.stem(token) for token in tokens]
 
-def retrieve_important(tree: etree.ElementTree):
+def retrieve_important(tree: etree.ElementTree) -> set[str]:
     # Retrieve the unique tokens from the bold, header, and title tags
-    return set(tokenize(''.join(tree.xpath("//b|//title|//h"))))
+    return set(porter_stem(tokenize(''.join(tree.xpath("//b|//title|//h")))))
 
-def retrieve_content(tree: etree.ElementTree):
+def retrieve_content(tree: etree.ElementTree) -> list[str]:
     # Get all text content
     return tokenize(''.join(tree.xpath("//text()")))
