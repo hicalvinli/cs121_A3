7c924eec8f03690d0373d9acf96f77ff186a8e9e
Anver Chou <anver.chou.123@gmail.com>
2025-02-26 00:08:02 -0800

    WindowsOS :(

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ def main():
     global PFILE_COUNT
     global INDEX
     relative = "rsrc/DEV-2"
+    # I use windows so i cant use rsrc :(
+    # relative = "C:\\Users\\anver\\Downloads\\dev-2\\DEV"
 
     # Iterate through every folder
     for folder in os.listdir(relative):
